75c264254d688fc2bf8a0460b5f8d050d0f7bb6c
ksdyb.park <ksdyb@amazon.com>
2022-05-26 11:06:37 +0900

    update

--- cdk-iot/lib/cdk-iot-stack.ts
+++ cdk-iot/lib/cdk-iot-stack.ts
@@ -1,16 +1,268 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as kinesisstream from 'aws-cdk-lib/aws-kinesis';
+import * as kinesisfirehose from 'aws-cdk-lib/aws-kinesisfirehose';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
+import * as cfn from 'aws-cdk-lib/aws-cloudformation';
+import * as logs from 'aws-cdk-lib/aws-logs'
+
+import * as glue from 'aws-cdk-lib/aws-glue'
+import * as athena from 'aws-cdk-lib/aws-athena'
 
 export class CdkIotStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // S3
+    const s3Bucket = new s3.Bucket(this, "cdk-businfo",{
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      versioned: false,
+    });
+    new cdk.CfnOutput(this, 'bucketName', {
+      value: s3Bucket.bucketName,
+      description: 'The nmae of bucket',
+    });
+    new cdk.CfnOutput(this, 's3Arn', {
+      value: s3Bucket.bucketArn,
+      description: 'The arn of s3',
+    });
+    new cdk.CfnOutput(this, 's3Path', {
+      value: 's3://'+s3Bucket.bucketName,
+      description: 'The path of s3',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkIotQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // kinesis data stream
+    const stream = new kinesisstream.Stream(this, 'Stream', {
+      streamName: 'businfo',
+      retentionPeriod: cdk.Duration.hours(48),
+      streamMode: kinesisstream.StreamMode.ON_DEMAND
+    });
+    new cdk.CfnOutput(this, 'StreamARN', {
+      value: stream.streamArn,
+      description: 'The arn of kinesis stream',
+    });
+
+    // using pre-defined metric method
+    stream.metricGetRecordsSuccess();
+    stream.metricPutRecordSuccess();
+
+    // DynamoDB
+    const tableName = 'dynamodb-businfo';
+    const dataTable = new dynamodb.Table(this, 'dynamodb-businfo', {
+        tableName: tableName,
+        partitionKey: { name: 'RouteId', type: dynamodb.AttributeType.STRING },
+        sortKey: { name: 'Timestamp', type: dynamodb.AttributeType.STRING },
+        billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        //timeToLiveAttribute: 'ttl',
+        kinesisStream: stream,
+    });
+
+    // Lambda - kinesisInfo
+    const lambdakinesis = new lambda.Function(this, "LambdaKinesisStream", {
+      description: 'get eventinfo from kinesis data stream',
+      runtime: lambda.Runtime.NODEJS_14_X, 
+      code: lambda.Code.fromAsset("repositories/lambda-kinesis-stream"), 
+      handler: "index.handler", 
+      timeout: cdk.Duration.seconds(3),
+      environment: {
+      }
+    }); 
+
+    // Lambda - kinesisInfo
+    const lambdafirehose = new lambda.Function(this, "LimbdaKinesisFirehose", {
+      description: 'update event sources',
+      runtime: lambda.Runtime.NODEJS_14_X, 
+      code: lambda.Code.fromAsset("repositories/lambda-kinesis-firehose"), 
+      handler: "index.handler", 
+      timeout: cdk.Duration.seconds(3),
+      environment: {
+      }
+    }); 
+    new cdk.CfnOutput(this, 'LambdaKinesisARN', {
+      value: lambdafirehose.functionArn,
+      description: 'The arn of lambda for kinesis',
+    });
+
+    // connect lambda for kinesis with kinesis data stream
+    const eventSource = new lambdaEventSources.KinesisEventSource(stream, {
+      startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+    });
+    lambdakinesis.addEventSource(eventSource);    
+
+    // Lambda - UpdateBusInfo
+    const lambdaBusInfo = new lambda.Function(this, "LambdaBusInfo", {
+      description: 'Lambda for businfo',
+      runtime: lambda.Runtime.NODEJS_14_X, 
+      code: lambda.Code.fromAsset("repositories/lambda-businfo"), 
+      handler: "index.handler", 
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        tableName: tableName,
+      }
+    });  
+    dataTable.grantReadWriteData(lambdaBusInfo);
+
+    // cron job - EventBridge
+    const rule = new events.Rule(this, 'Cron', {
+      description: "Schedule a Lambda to save arrival time of buses",
+      schedule: events.Schedule.expression('rate(1 minute)'),
+    }); 
+    rule.addTarget(new targets.LambdaFunction(lambdaBusInfo));
+
+    // generate a table by crawler 
+    const crawlerRole = new iam.Role(this, "crawlerRole", {
+      assumedBy: new iam.ServicePrincipal("glue.amazonaws.com"),
+      description: "Role for parquet translation",
+    });
+    crawlerRole.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole',
+    });  
+    crawlerRole.addToPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "s3:AbortMultipartUpload",
+        "s3:GetBucketLocation",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:ListBucketMultipartUploads",
+        "s3:PutObject"
+      ],
+      resources: [
+        s3Bucket.bucketArn,
+        s3Bucket.bucketArn + "/*"
+      ],
+    }));
+    new cdk.CfnOutput(this, 'crawlerRoleArn', {
+      value: crawlerRole.roleArn,
+      description: 'The arn of crawlerRole',
+    });
+    
+    // crawler 
+    const glueDatabaseName = "businfo";
+    const crawler = new glue.CfnCrawler(this, "TranslateToParquetGlueCrawler", {
+      name: "translate-parquet-crawler",
+      role: crawlerRole.roleArn,
+      targets: {
+          s3Targets: [
+              {path: 's3://'+s3Bucket.bucketName+'/businfo'}, 
+          ]
+      },
+      databaseName: glueDatabaseName,
+      schemaChangePolicy: {
+          deleteBehavior: 'DELETE_FROM_DATABASE'
+      },      
+    });
+
+    // Traslation Role
+    const translationRole = new iam.Role(this, 'TranslationRole', {
+      assumedBy: new iam.ServicePrincipal("firehose.amazonaws.com"),
+      
+      description: 'TraslationRole',
+    });
+    translationRole.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/AWSLambdaExecute',
+    });  
+    translationRole.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AWSLambdaKinesisExecutionRole',
+    });
+    translationRole.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole',
+    });
+    translationRole.addToPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "s3:AbortMultipartUpload",
+        "s3:GetBucketLocation",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:ListBucketMultipartUploads",
+        "s3:PutObject"
+      ],
+      resources: [
+        s3Bucket.bucketArn,
+        s3Bucket.bucketArn + "/*"
+      ],
+    }));
+    translationRole.addToPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "lambda:InvokeFunction", 
+        "lambda:GetFunctionConfiguration", 
+      ],
+      resources: [
+        lambdafirehose.functionArn, 
+        lambdafirehose.functionArn+':*'],
+    }));
+    translationRole.addToPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "glue:GetTable",
+        "glue:GetTableVersion",
+        "glue:GetTableVersions"
+      ],
+      resources: ['*'],
+    }));
+
+    const firehose = new kinesisfirehose.CfnDeliveryStream(this, 'FirehoseDeliveryStream', {
+      deliveryStreamType: 'KinesisStreamAsSource',
+      kinesisStreamSourceConfiguration: {
+        kinesisStreamArn: stream.streamArn,
+        roleArn: translationRole.roleArn,
+      },      
+      extendedS3DestinationConfiguration: {
+        bucketArn: s3Bucket.bucketArn,
+        bufferingHints: {
+          intervalInSeconds: 60,
+          sizeInMBs: 128    // mininum 64MBs at data format conversion 
+        },
+        compressionFormat: 'UNCOMPRESSED', // GZIP, SNAPPY
+        encryptionConfiguration: {
+          noEncryptionConfig: "NoEncryption"
+        },
+        prefix: "businfo/",
+        errorOutputPrefix: 'eror/',
+        roleArn: translationRole.roleArn,
+        processingConfiguration: {
+          enabled: true,
+          processors: [{
+            type: 'Lambda',
+              parameters: [{
+              parameterName: 'LambdaArn',
+              parameterValue: lambdafirehose.functionArn
+            }]
+          }]
+        }, 
+        dataFormatConversionConfiguration: {          
+          enabled: false, 
+          schemaConfiguration: {
+            databaseName: glueDatabaseName, // Target Glue database name
+            roleArn: translationRole.roleArn,
+            tableName: 'businfo' // Target Glue table name
+          }, 
+        }, 
+      }
+    });      
+
+    // athena workgroup
+  /*  new athena.CfnWorkGroup(this, 'analytics-athena-workgroup', {
+      name: `businfo-workgroup`,
+      workGroupConfiguration: {
+        resultConfiguration: {
+          outputLocation: `s3://${s3Bucket.bucketName}`,
+        },
+      },
+    }) */
+  }
   }
 }

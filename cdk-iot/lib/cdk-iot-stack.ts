13230115bf4577a01918efa1552d9b46a36fbbcb
ksdyb.park <ksdyb@amazon.com>
2022-06-01 11:08:41 +0900

    update

--- cdk-iot/lib/cdk-iot-stack.ts
+++ cdk-iot/lib/cdk-iot-stack.ts
@@ -4,10 +4,16 @@ import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment"
 import * as events from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
 import * as kinesisstream from 'aws-cdk-lib/aws-kinesis';
 import * as kinesisfirehose from 'aws-cdk-lib/aws-kinesisfirehose';
+import {SnsEventSource} from 'aws-cdk-lib/aws-lambda-event-sources'
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
 import * as cfn from 'aws-cdk-lib/aws-cloudformation';
@@ -15,6 +21,13 @@ import * as logs from 'aws-cdk-lib/aws-logs';
 import * as glue from 'aws-cdk-lib/aws-glue'
 import * as athena from 'aws-cdk-lib/aws-athena'
 import * as iot from 'aws-cdk-lib/aws-iot';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import { IResource, LambdaIntegration, MockIntegration, PassthroughBehavior, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import * as cloudFront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+
+import { CloudFrontToApiGateway } from '@aws-solutions-constructs/aws-cloudfront-apigateway';
+
 
 export class CdkIotStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -22,6 +35,8 @@ export class CdkIotStack extends Stack {
 
     // S3
     const s3Bucket = new s3.Bucket(this, "themometer",{
+      bucketName: "storage-themometer",
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       publicReadAccess: false,
@@ -40,6 +55,27 @@ export class CdkIotStack extends Stack {
       description: 'The path of s3',
     });
 
+    new s3Deploy.BucketDeployment(this, "DeployReactApp", {
+      sources: [s3Deploy.Source.asset("../webcliet")],
+      destinationBucket: s3Bucket,
+    })
+
+    // cloudfront
+    const distribution = new cloudFront.Distribution(this, 'themometer', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(s3Bucket),
+        allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,        
+        viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,        
+      },
+      priceClass: cloudFront.PriceClass.PRICE_CLASS_200,  
+      enableLogging: true,
+    //  description: 'cdk cloudFront',
+    });
+    new cdk.CfnOutput(this, 'distributionDomainName', {
+      value: distribution.domainName,
+      description: 'The domain name of the Distribution',
+    });
+    
     // kinesis data stream
     const streamName = 'themometer';
     const stream = new kinesisstream.Stream(this, 'Stream', {
@@ -56,6 +92,16 @@ export class CdkIotStack extends Stack {
     stream.metricGetRecordsSuccess();
     stream.metricPutRecordSuccess();
 
+    // SNS
+    const topic = new sns.Topic(this, 'sns-iot', {
+      topicName: 'sns-iot'
+    });
+    topic.addSubscription(new subscriptions.EmailSubscription('storytimebot21@gmail.com'));
+    new cdk.CfnOutput(this, 'snsTopicArn', {
+      value: topic.topicArn,
+      description: 'The arn of the SNS topic',
+    });
+
     // Lambda for stream 
     const lambdaStream = new lambda.Function(this, "LambdaKinesisStream", {
       description: 'get eventinfo from kinesis data stream',
@@ -64,6 +110,7 @@ export class CdkIotStack extends Stack {
       handler: "index.handler", 
       timeout: cdk.Duration.seconds(3),
       environment: {
+        topicArn: topic.topicArn
       }
     }); 
 
@@ -72,6 +119,18 @@ export class CdkIotStack extends Stack {
       startingPosition: lambda.StartingPosition.TRIM_HORIZON,
     });
     lambdaStream.addEventSource(eventSource);  
+
+    // Lambda - Slack
+    const lambdaSlack = new lambda.Function(this, "LambdaSlack", {
+      runtime: lambda.Runtime.NODEJS_14_X, 
+      code: lambda.Code.fromAsset("../lambda-for-slack"), 
+      handler: "index.handler", 
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        token: "sample-3205298473938-3233090599600-5Kr8k7W8dieUwoL5d7GekmpJ"
+      }
+    });    
+    lambdaSlack.addEventSource(new SnsEventSource(topic));     
 
     // Rule Role for IoT
     const ruleRole = new iam.Role(this, "ruleRole", {
@@ -350,16 +409,6 @@ export class CdkIotStack extends Stack {
         `arn:aws:logs:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:*`
       ],
     }));
-    athenaRole.addToPolicy(new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      actions: [
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      resources: [
-        `arn:aws:logs:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:*`
-      ],
-    }));
     new cdk.CfnOutput(this, 'AthenaRoleArn', {
       value: athenaRole.roleArn,
       description: 'The arn of AthenaRole',
@@ -383,5 +432,181 @@ export class CdkIotStack extends Stack {
       value: lambdaAthena.functionArn,
       description: 'The arn of lambda for athena',
     });
+
+    // API GATEWAY
+    const stage = "dev";
+
+    // log group api
+    const logGroup = new logs.LogGroup(this, 'AccessLogs', {
+      retention: 90, // Keep logs for 90 days
+    });
+    logGroup.grantWrite(new iam.ServicePrincipal('apigateway.amazonaws.com')); 
+
+    // api-role
+    const role = new iam.Role(this, "api-role", {
+      roleName: "ApiRole",
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com")
+    });
+    role.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: ['lambda:InvokeFunction']
+    }));
+    role.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/AWSLambdaExecute',
+    }); 
+
+    // define api gateway
+    const api = new apiGateway.RestApi(this, 'api-thermometer', {
+      description: 'API Gateway',
+      endpointTypes: [apiGateway.EndpointType.REGIONAL],
+    /*  defaultCorsPreflightOptions: {
+        allowOrigins: apiGateway.Cors.ALL_ORIGINS,
+        allowMethods: apiGateway.Cors.ALL_METHODS // this is also the default
+      }, */
+      binaryMediaTypes: ['*/*'], 
+      deployOptions: {
+        stageName: stage,
+        accessLogDestination: new apiGateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
+          caller: false,
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          user: true
+        }),
+      },
+    //  proxy: false
+    });   
+
+    lambdaAthena.grantInvoke(new iam.ServicePrincipal('apigateway.amazonaws.com'));
+
+    const templateString: string = `##  See http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+    ##  This template will pass through all parameters including path, querystring, header, stage variables, and context through to the integration endpoint via the body/payload
+    #set($allParams = $input.params())
+    {
+    "body-json" : $input.json('$'),
+    "params" : {
+    #foreach($type in $allParams.keySet())
+        #set($params = $allParams.get($type))
+    "$type" : {
+        #foreach($paramName in $params.keySet())
+        "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
+            #if($foreach.hasNext),#end
+        #end
+    }
+        #if($foreach.hasNext),#end
+    #end
+    },
+    "stage-variables" : {
+    #foreach($key in $stageVariables.keySet())
+    "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
+        #if($foreach.hasNext),#end
+    #end
+    },
+    "context" : {
+        "account-id" : "$context.identity.accountId",
+        "api-id" : "$context.apiId",
+        "api-key" : "$context.identity.apiKey",
+        "authorizer-principal-id" : "$context.authorizer.principalId",
+        "caller" : "$context.identity.caller",
+        "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
+        "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
+        "cognito-identity-id" : "$context.identity.cognitoIdentityId",
+        "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
+        "http-method" : "$context.httpMethod",
+        "stage" : "$context.stage",
+        "source-ip" : "$context.identity.sourceIp",
+        "user" : "$context.identity.user",
+        "user-agent" : "$context.identity.userAgent",
+        "user-arn" : "$context.identity.userArn",
+        "request-id" : "$context.requestId",
+        "resource-id" : "$context.resourceId",
+        "resource-path" : "$context.resourcePath"
+        }
+    }`    
+    const requestTemplates = { // path through
+      "image/jpeg": templateString,
+      "image/jpg": templateString,
+      "application/octet-stream": templateString,
+      "image/png" : templateString
+    }
+    
+    const status = api.root.addResource('upload');
+
+    status.addMethod('POST', new apiGateway.LambdaIntegration(lambdaAthena, {
+      passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,  // options: NEVER
+      credentialsRole: role,
+      requestTemplates: requestTemplates,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:false, 
+    }), {
+      methodResponses: [   // API Gateway sends to the client that called a method.
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+
+    addCorsOptions(status);
+    
+    new cdk.CfnOutput(this, 'apiUrl', {
+      value: api.url,
+      description: 'The url of API Gateway',
+    });
+
+
+  /*  const apiGateway2 = new apiGateway.LambdaRestApi(this, 'LambdaRestApi', {
+      handler: lambdaAthena,
+      endpointConfiguration: {
+        types: [apiGateway.EndpointType.REGIONAL]
+      },
+      defaultMethodOptions: {
+        authorizationType: apiGateway.AuthorizationType.NONE
+      }
+    });
+    new CloudFrontToApiGateway(this, 'test-cloudfront-apigateway', {
+      existingApiGatewayObj: apiGateway2
+    }); */
   }
 }
+
+export function addCorsOptions(apiResouce: IResource) {
+  // CORS
+  apiResouce.addMethod('OPTIONS', new MockIntegration({
+    integrationResponses: [{ 
+      statusCode: '200',
+        responseParameters: {
+          'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+          'method.response.header.Access-Control-Allow-Origin': "'*'",
+          'method.response.header.Access-Control-Allow-Credentials': "'false'",
+          'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
+        }
+      },
+    ],
+    passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_MATCH,
+    requestTemplates: {
+      'application/json': '{ "statusCode": 200 }',
+    },
+  }), {
+    methodResponses: [{ 
+        statusCode: '200',
+        responseParameters: {
+          'method.response.header.Access-Control-Allow-Headers': true,
+          'method.response.header.Access-Control-Allow-Methods': true,
+          'method.response.header.Access-Control-Allow-Credentials': true,
+          'method.response.header.Access-Control-Allow-Origin': true,
+        }, 
+      },
+    ],
+  });   
+}
+
